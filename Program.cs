7cfc47185c8499edb54774459bb4919b86f1cfa1
Enyel Jovier Torres S. <enyelsantana1001@gmail.com>
2026-02-27 14:37:47 -0400

    Nuevo Repositorio

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FlujoDeControl2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string estudiante, estatus;
+            double n1, n2, n3, n4, promedio, suma;
+            int cantidadestudiantes;
+            string fila = "";
+
+            Console.Write("Ingrese la cantidad de estudiantes : ");
+            cantidadestudiantes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= cantidadestudiantes; i++)
+            {
+                Console.Write("\nIngrese el nombre del estudiante : ");
+                estudiante = Console.ReadLine();
+
+                Console.Write("Ingrese la nota 1 : ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese la nota 2 : ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese la nota 3 : ");
+                n3 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese la nota 4 : ");
+                n4 = Convert.ToDouble(Console.ReadLine());
+
+                suma = n1 + n2 + n3 + n4;
+                promedio = suma / 4;
+
+                if (promedio >= 70)
+                    estatus = "Aprobado";
+
+                else
+                    estatus = "Reprobado";
+
+                
+                fila += $"{estudiante}\t\t{n1}\t{n2}\t{n3}\t{n4}\t{promedio}\t\t{estatus}\n";
+            }
+
+            Console.WriteLine("\n------------------------------------------------------------------------");
+            Console.WriteLine("Estudiantes\tNota1\tNota2\tNota3\tNota4\tPromedio\tEstatus");
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            Console.WriteLine(fila);
+
+            Console.WriteLine("------------------------------------------------------------------------");
+        }
+    }
+}
